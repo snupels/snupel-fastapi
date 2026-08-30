a9f6b67f0f76a1e6767638570bc5fad1fd4626fa
dguie86 <dguie86@snu.ac.kr>
2026-08-30 16:43:53 +0900

    fix: allow oauth credential requests

--- app/main.py
+++ app/main.py
@@ -30,6 +30,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://sportspassport.kr"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
